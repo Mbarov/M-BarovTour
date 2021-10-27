4df4f73508ffbc3771db643158e5b0960392a44f
Mbarov <prom@tgv73.ru>
2021-10-27 21:46:51 +0400

    1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect,  url_for
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 from cloudipsp import Api, Checkout #подключение системы оплаты от fondy. Перед этим прописать pip install cloudipsp
